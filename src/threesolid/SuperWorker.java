1c75c566103cb60318165106cdb0bffbbb76c4a9
jkj49 <52798397+jkj49@users.noreply.github.com>
2019-09-10 19:33:11 -0500

    Update SuperWorker.java

--- src/threesolid/SuperWorker.java
+++ src/threesolid/SuperWorker.java
@@ -1,6 +1,15 @@
 package threesolid;
 
-class SuperWorker implements IWorker{
+
+/**
+ * S - A SuperWorker does work and eats.
+ * O - SuperWorker can be extended.
+ * I - A SuperWorker eats and works, implementing IWorkable and IFeedable.
+ * Jonathan Joiner
+ *
+ */
+
+class SuperWorker implements IWorkable, IFeedable{
 	public void work() {
 		//.... working much more
 	}
