9438520f33c31d43ddd354361013af1e2aa46905
kbl35 <54654620+kbl35@users.noreply.github.com>
2019-09-12 17:13:04 -0500

    comment

--- src/threesolid/IFeedable.java
+++ src/threesolid/IFeedable.java
@@ -1,7 +1,7 @@
 package threesolid;
 /**
  * S - Has the single responsibilty of requiring IFeedable clients to implement a work method.
- * O - By using an interface something can work, but it can also be extended to do more things.
+ * O - By using an interface, the program will be open for extending more functionalities which might be needed in future.
  * I - Robots don't eat, but they need something else like maintaince.
  *
  * Ky_Le
