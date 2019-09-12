f08b75ae82f89c19515163bf74f8862673b3cc57
Jacob M. Hope <jacobmhope@gmail.com>
2019-09-12 13:02:58 -0500

    Add manage human and robot tests

--- src/threesolid/ThreeSolidMain.java
+++ src/threesolid/ThreeSolidMain.java
@@ -21,7 +21,7 @@ public class ThreeSolidMain
 
       try
       {
-         System.out.format("Starting ... \n");
+         System.out.format("Starting ... \n\n");
       }
       catch (Exception main_except)
       {
@@ -31,16 +31,24 @@ public class ThreeSolidMain
       System.out.println(" Testing for HumanWorker class");
       tsHuman.eat();
       tsHuman.work();
+      System.out.println();
 
       System.out.println(" Testing class Robot working properly");
       tsRobot.work();
       tsRobot.processMaintainWeekly();
       tsRobot.processMaintainMonthly();
       tsRobot.processMaintainYearly();
+      System.out.println();
 
-      System.out.println(" Testing Manager class");
+      System.out.println("Testing Manager class");
+      System.out.println("Manage Human...");
       tsManager.setWorker(tsHuman);
       tsManager.manage();
+      System.out.println("Manage Robot...");
+      tsManager.setWorker(tsRobot);
+      tsManager.manage();
+      System.out.println("Test Complete");
+      System.out.println();
 
       try
       {
