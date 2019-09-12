ed3c68e3cc1a2302ecb4dea9a96fe99d0e5f559c
kbl35 <54654620+kbl35@users.noreply.github.com>
2019-09-12 17:09:56 -0500

    yes

--- src/threesolid/IFeedable.java
+++ src/threesolid/IFeedable.java
@@ -1,7 +1,11 @@
 package threesolid;
-
-// interface Segregation principle because robot will not eat but robot will need something else.
-// Ky_Le
+/**
+ * S - Has the single responsibilty of requiring IFeedable clients to implement a work method.
+ * O - By using an interface something can work, but it can also be extended to do more things.
+ * I - Robots don't eat, but they need something else like maintaince.
+ *
+ * Ky_Le
+ */
 
 interface IFeedable{
 	public void eat();
